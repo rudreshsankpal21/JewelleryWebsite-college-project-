6d9f5e4c7b20030b627a5e61a0adc15750414563
Rudresh <rudreshsankpal21@gmail.com>
2026-02-20 20:57:25 +0530

    refactor : change height of the hero section banner

--- my-react-app/src/Components/HeroSection.jsx
+++ my-react-app/src/Components/HeroSection.jsx
@@ -1,7 +1,7 @@
 const HeroSection = () => {
   return (
     <div>
-      <div className="hero-section-img" style={{height:'85vh'}}>
+      <div className="hero-section-img" style={{height:'80vh', marginTop:'120px'}}>
         <img src="./src/assets/hero-banner.png" alt="banner-img" height={'100%'} width={'100%'}/>
       </div>
     </div>
