04132f005da2a5349de491f3298eeaab089cbebe
Rudresh <rudreshsankpal21@gmail.com>
2026-03-08 23:58:38 +0530

    add new route path for navigating

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MetalRatesPage from "./pages/MetalRatesPage";
-import AboutPage from "./pages/AboutPage";
+import AboutPage from "./pages/Aboutpage";
+import ProductsPage from "./pages/Productspage";
+import ContactPage from "./pages/Contactpage";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/metal-rates" element={<MetalRatesPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
   );
